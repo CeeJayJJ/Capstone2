3bd1368c7e49d5932137975a79b89e819bbf887d
ceejayjj21 <charlierudolf44@gmail.com>
2024-09-02 22:13:03 +0800

    Assets

---
+++ Assets/Script/LoadingScreen.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject LoadingScreens;
+   
+    public Image LoadingBarFill;
+
+    public void LoadScene(int SceneID)
+    {
+       StartCoroutine(LoadSceneAsync(SceneID));
+
+    }
+
+    IEnumerator LoadSceneAsync(int SceneID)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneID);
+
+        LoadingScreens.SetActive(true);
+
+        while(!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            LoadingBarFill.fillAmount = progressValue;
+
+            yield return null;
+
+        }
+    }
+    
+}
