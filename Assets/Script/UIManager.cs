727f41ecd1e14a131034f4e9607202ee7c8e00f7
jromarph <153130622+macedajuanromar@users.noreply.github.com>
2024-09-17 05:47:40 +0800

    additional core scripts

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; } // Singleton
+
+    // References to UI elements (panels, text, images, etc.)
+    public GameObject dialoguePanel;
+    public TMPro.TextMeshProUGUI dialogueText;
+    public GameObject questLogPanel;
+    public GameObject inventoryPanel;
+    // ... other UI elements
+
+    private void Awake()
+    {
+        // Singleton implementation (similar to other core scripts)
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    // Methods to show/hide UI elements
+    public void ShowDialogue(string text)
+    {
+        dialoguePanel.SetActive(true);
+        dialogueText.text = text;
+    }
+
+    public void HideDialogue()
+    {
+        dialoguePanel.SetActive(false);
+    }
+
+    public void ShowQuestLog()
+    {
+        questLogPanel.SetActive(true);
+        // Populate quest log UI with data from QuestManager
+    }
+
+    public void HideQuestLog()
+    {
+        questLogPanel.SetActive(false);
+    }
+
+    public void ShowInventory()
+    {
+        inventoryPanel.SetActive(true);
+        // Populate inventory UI with data from InventoryManager
+    }
+
+    public void HideInventory()
+    {
+        inventoryPanel.SetActive(false);
+    }
+
+    // ... other UI management methods
+}
