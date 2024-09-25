84c5852424ceb119d948a3900362c699e1204f0e
ceejayjj21 <charlierudolf44@gmail.com>
2024-09-25 12:07:17 +0800

    Work In Progress #1

---
+++ Assets/For Testing/CameraFades.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFades : MonoBehaviour
+{
+
+    private FadeObject _fader;
+
+   
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if(player !=null)
+        {
+            Vector3 dir = player.transform.position - transform.position;
+            Ray ray = new Ray(transform.position, dir);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider == null)
+                    return;
+
+                if(hit.collider.gameObject == player)
+                {
+                    if(_fader != null)
+                    {
+                        _fader.DoFade = false;
+                    }
+                }
+                else
+                {
+                    _fader = hit.collider.gameObject.GetComponent<FadeObject>();
+                    if(_fader != null )
+                    {
+                        _fader.DoFade = true;
+                    }
+                }
+                
+            }
+        }
+    }
+}
