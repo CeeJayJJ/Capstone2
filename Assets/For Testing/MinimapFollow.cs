c9cae11b4e72d83d9799a68f0a29ce4ed169c662
jromarph <153130622+macedajuanromar@users.noreply.github.com>
2024-11-21 23:15:39 +0800

    MINIMAP BUG SOLVED!

--- Assets/For Testing/MinimapFollow.cs
+++ Assets/For Testing/MinimapFollow.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class MinimapFollow : MonoBehaviour
 {
-    public Transform player;
-
     void LateUpdate()
     {
-        // Follow the player, maintaining a fixed height
-        Vector3 newPosition = player.position;
-        newPosition.y = transform.position.y;
-        transform.position = newPosition;
+        // Find the GameObject with the "Player" tag
+        GameObject playerObject = GameObject.FindWithTag("Player");
+
+        // Check if the playerObject was found
+        if (playerObject != null)
+        {
+            // Get the player's position
+            Vector3 playerPosition = playerObject.transform.position;
+
+            // Create a new position for the camera or object, maintaining a fixed height
+            Vector3 newPosition = playerPosition;
+            newPosition.y = transform.position.y; // Maintain the current height of this object
+
+            // Update the position of this object
+            transform.position = newPosition;
+        }
+        else
+        {
+            Debug.LogWarning("Player GameObject not found.");
+        }
     }
 }
--- Assets/For Testing/MinimapIcon.cs
+++ Assets/For Testing/MinimapIcon.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class MinimapIcon : MonoBehaviour
 {
-    public Transform player;
-
+   
     void LateUpdate()
     {
-        // Match the player's Y-axis rotation
-        Vector3 rotation = transform.eulerAngles;
-        rotation.y = player.eulerAngles.y;
-        transform.eulerAngles = rotation;
+        // Find the GameObject with the "Player" tag
+        GameObject playerObject = GameObject.FindWithTag("Player");
+
+        // Check if the playerObject was found
+        if (playerObject != null)
+        {
+            // Get the player's rotation
+            Vector3 playerRotation = playerObject.transform.eulerAngles;
+
+            // Set this object's rotation to match the player's y rotation
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, playerRotation.y, transform.eulerAngles.z);
+        }
+        else
+        {
+            Debug.LogWarning("Player GameObject not found.");
+        }
     }
 }
