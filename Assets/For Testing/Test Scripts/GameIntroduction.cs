e74d24aca255cc157d4257f2f23a1f2bf4406613
jromarph <153130622+macedajuanromar@users.noreply.github.com>
2024-11-26 00:36:14 +0800

    achievements but okay

--- Assets/For Testing/Test Scripts/GameIntroduction.cs
+++ Assets/For Testing/Test Scripts/GameIntroduction.cs
@@ -7,9 +7,11 @@ public class GameIntroduction : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
+        AchievementsManager.instance.UnlockAchievement(Achievements.FirstPlay);
         SceneManager.LoadScene("Introduction",LoadSceneMode.Single);
         PlayerPrefs.SetString("LastExitName", "Room");
         AudioManager.Instance.musicSource.Stop();
         AudioManager.Instance.PlayMusic("Ambience_House");
+        
     }
 }
